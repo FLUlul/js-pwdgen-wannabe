8b5fbc11eb256a5f5db041e390711ac1f860c437
FLU <valerio.massj@gmail.com>
2021-10-21 12:55:53 +0200

    added comments and cleared the password variable

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21 */
 
-
+/* ask the utent his name */
 let name = prompt("Insert your name here");
+
+/* ask the utent his last name */
 let lastName = prompt("Insert your last name here");
+
+/* ask the utent his favorite color */
 let favoriteColor = prompt("Insert your name here");
 
-let password = `Your assigned password is: ${name}${lastName}${favoriteColor}21`;
+/* create the variable with the text+password */
+let password = `${name}${lastName}${favoriteColor}21`;
 
-document.getElementById("psw").innerHTML = password;
+/* print in html the password result */
+document.getElementById("psw").innerHTML = `Your assigned password is: ${password}`;
